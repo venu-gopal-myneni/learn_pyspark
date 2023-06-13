7afdd2e7f413fed6aa873fc3ed041790a3dc14fe
venu <mailvenumyneni@gmail.com>
2023-06-13 22:46:55 +0530

    Window

--- window.py
+++ window.py
@@ -1,5 +1,8 @@
-
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+from pyspark.sql.types import StructField, StructType,IntegerType,DateType
+from pyspark.sql.window import Window
+
 spark = SparkSession.builder.getOrCreate()
 # root
 #  |-- show_id: string (nullable = true)
@@ -18,10 +21,9 @@ spark = SparkSession.builder.getOrCreate()
 
 df = spark.read.format("csv").option("header", True).option("inferSchema", True).load(r"data\archive\netflix_titles.csv")
 
-df.explain()
-df.cache()
-df.explain()
-
-# get schema
-df.printSchema()
-print(df.schema)
+# Get countrywise ranks based on year released
+dfr = df.withColumn("release_year",f.col("release_year").cast(IntegerType()))
+dfr.printSchema()
+rank_window = Window.partitionBy("country").orderBy("release_year")
+dfr2= dfr.withColumn("country_wise_rank",f.dense_rank().over(rank_window))
+dfr2.select("country","release_year","country_wise_rank","title").show()
